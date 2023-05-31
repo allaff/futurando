cdfbfbddc33d06cd254dc7e07c216dfa7cd426ba
Allaff Gustavo <allaff.ribeiro@academico.ifpb.edu.br>
2023-05-31 11:10:40 -0300

    adicionei o componente no page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import { Header } from '@/components/Header'
+
 export default function Home() {
   return (
     <>
       <h1>Teste 1</h1>
       <p>Testando do meu para o remoto</p>
       <p>Testando</p>
+      <Header />
     </>
   )
 }
